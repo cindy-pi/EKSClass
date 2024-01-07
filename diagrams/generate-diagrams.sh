f5c58b899b061aba0587078d421652866b407e62
Cindy Sullivan <cindy.sullivan@skyloch.com>
2024-01-07 17:08:42 -0500

    Working on documentation

---
+++ diagrams/generate-diagrams.sh
@@ -0,0 +1,9 @@
+drawio=/Applications/draw.io.app/Contents/MacOS/draw.io
+
+$drawio --format svc -p 1 -o diag-1.svg -x eks-overview.drawio
+$drawio --format svc -p 2 -o diag-2.svg -x eks-overview.drawio
+$drawio --format svc -p 3 -o diag-3.svg -x eks-overview.drawio
+$drawio --format svc -p 4 -o diag-4.svg -x eks-overview.drawio
+$drawio --format svc -p 5 -o diag-5.svg -x eks-overview.drawio
+$drawio --format svc -p 6 -o diag-6.svg -x eks-overview.drawio
+
